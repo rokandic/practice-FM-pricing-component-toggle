136b7ba215dec7d960eb2689f30c40de6faa5fc8
rokandic <155958535+rokandic@users.noreply.github.com>
2024-03-29 08:29:59 +0100

    Solution using React

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // DODAJ: base: '/<REPO>/' primer: '/practice-FM-pricing-component-toggle/'
   base: '/practice-FM-pricing-component-toggle/', 
 })
